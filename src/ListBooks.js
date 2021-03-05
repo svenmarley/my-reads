b2b438c2153f6953aeb9d29b4fd6c1c295a9eed8
Mike Anderson <mikea@candicontrols.com>
2021-03-05 15:40:18 -0800

    Added  comments to the code.
    Moved some of the Formatting from App.js into this ListBooks component
    
    Removed excess code

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Book from './Book';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ListBooks extends Component {
     static propTypes = {
@@ -21,32 +22,43 @@ class ListBooks extends Component {
             <div>
                 <div className="list-books-content">
                     {!isSearchList ? (
-                        shelves.filter( ( s ) => {
-                            return ( s.isDisplayed );
-                        } )
-                               .map( ( shelf ) => (
-                                   <div className="bookshelf" key={shelf.id}>
-                                       <h2 className="bookshelf-title">{shelf.text}</h2>
-                                       <div className="bookshelf-books">
-                                           {books.length ? (
-                                               <ol className="books-grid">
-                                                   {books.filter( ( book ) => {
-                                                       return ( ( book.shelf === shelf.apiID ) );
-                                                   } ).map( ( book ) => (
-                                                       <Book
-                                                           key={book.id}
-                                                           book={book}
-                                                           updateBook={updateBook}
-                                                       />
-                                                   ) )
-                                                   }
-                                               </ol>
-                                           ) : (
-                                               <span>No books found</span>
-                                           )}
-                                       </div>
-                                   </div>
-                               ) )
+                        <div className="list-books">
+                            <div className="list-books-title">
+                                <h1>MyReads</h1>
+                            </div>
+                            {
+                                shelves.filter( ( s ) => {      // just get shelves that are supposed to be displayed (i.e. not NONE)
+                                    return ( s.isDisplayed );
+                                } ).map( ( shelf ) => (
+                                    <div className="bookshelf" key={shelf.id}>
+                                        <h2 className="bookshelf-title">{shelf.text}</h2>
+                                        <div className="bookshelf-books">
+                                            {books.length ? (       // check that there are books to display
+                                                <ol className="books-grid">
+                                                    {books.filter( ( book ) => {
+                                                        return ( ( book.shelf === shelf.apiID ) );
+                                                    } ).map( ( book ) => (
+                                                        <Book
+                                                            key={book.id}
+                                                            book={book}
+                                                            updateBook={updateBook}
+                                                        />
+                                                    ) )
+                                                    }
+                                                </ol>
+                                            ) : (
+                                                <span>No books found</span>
+                                            )}
+                                        </div>
+                                    </div>
+                                ) )
+                            }
+                            <div className="open-search">
+                                <Link to='/search'>
+                                    <button>Add a book</button>
+                                </Link>
+                            </div>
+                        </div>
 
                     ) : (
                         <div className="bookshelf" key='searchBookShelf'>
